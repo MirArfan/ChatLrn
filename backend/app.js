53e8269bc1ea496d538cf32e63db44c3522958b7
Mir Arfan Uddin <mir.arfan.iiuc9@gmail.com>
2025-12-06 12:25:12 +0600

    user login & authuser middleware

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import connect from "./db/db.js";
 import cors from 'cors';
+import cookieParser from "cookie-parser";
 import connectCloudinary from "./db/Cloudinary.js";
 import userRoutes from "./routes/user.routes.js";
 
@@ -14,12 +15,13 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // DB Connect here
 connect();
 connectCloudinary();
 
-app.use("/user", userRoutes);
+app.use("/users", userRoutes);
 
 app.get("/", (req, res) => {
     res.send("Hello World");
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import * as userController from '../controllers/user.controller.js'
+import * as authMiddleware from '../middleware/auth.middleware.js';
 import { body } from "express-validator";
 
 
@@ -10,5 +11,13 @@ router.post('/register',
     body('password').isLength({min: 3}).withMessage('password must be at least 3 characters'),
     userController.createUserController
 );
+
+router.post('/login',
+    body('email').isEmail().withMessage('Email must be valid email address'),
+    body('password').isLength({min: 3}).withMessage('password must be at least 3 characters'),
+    userController.loginController
+)
+
+router.get('/profile',authMiddleware.authUser, userController.profileController);
 
 export default router;
